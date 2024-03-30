157a69792e9ca24ea2d9b3ff6d42898446dd5b9a
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-03-30 08:05:58 +0300

    .

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import Currency from "./component/Currency";
 
 function App() {
   return (
-    <div className="flex flex-col items-center h-screen  bg-orange-200">
-      <h1 className="text-3xl font-bold">Currency converter</h1>
-      <Currency />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-orange-200 p-4">
+      <h1 className="text-3xl font-bold mb-4">Currency converter</h1>
+      <div className="w-full sm:w-auto">
+        <Currency />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// import "./App.css";
+// import Currency from "./component/Currency";
+
+// function App() {
+//   return (
+//     <div className="flex flex-col items-center justify-center h-screen  bg-orange-200">
+//       <h1 className="text-3xl font-bold">Currency converter</h1>
+//       <Currency />
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/component/Currency.jsx
+++ src/component/Currency.jsx
@@ -7,7 +7,7 @@ const Currency = () => {
   const [amount1, setAmount1] = useState("");
   const [amount2, setAmount2] = useState("");
   const [currency1, setCurrency1] = useState("USD");
-  const [currency2, setCurrency2] = useState("EUR");
+  const [currency2, setCurrency2] = useState("KES");
   const [exchangeRates, setExchangeRates] = useState({});
 
   useEffect(() => {
@@ -41,7 +41,48 @@ const Currency = () => {
     }
   }, [amount1, currency1, currency2, exchangeRates]);
 
-  return <div></div>;
+  return (
+    <div className="flex flex-row mt-8 gap-x-12">
+      {/* curr 1 */}
+      <div className="border-black border-2 rounded-lg p-1 bg-white">
+        <select
+          className="bg-black text-white rounded-lg pt-2 pb-2"
+          value={currency1}
+          onChange={(e) => setCurrency1(e.target.value)}
+        >
+          {Object.keys(exchangeRates).map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </select>
+        <input
+          className="ml-3 border-b-2 border-blue-700 outline-none"
+          type="number"
+          value={amount1}
+          onChange={(e) => setAmount1(e.target.value)}
+        />
+      </div>
+      {/* curr 2 */}
+      <div className="border-black border-2 rounded-lg p-1 bg-white">
+        <select
+          className="bg-black text-white rounded-lg pt-2 pb-2"
+          value={currency2}
+          onChange={(e) => setCurrency2(e.target.value)}
+        >
+          {Object.keys(exchangeRates).map((country) => (
+            <option key={country}>{country}</option>
+          ))}
+        </select>
+        <input
+          className="ml-3 border-b-2 border-blue-700 outline-none"
+          type="number"
+          value={amount2}
+          onChange={(e) => setAmount2(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Currency;
